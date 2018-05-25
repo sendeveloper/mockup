6c97e8cd137a6a21d21ed1d25ef13c9c10bdd9cd
sendeveloper <lightmorning46@gmail.com>
2018-05-24 20:32:43 -0700

    updated

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -3,11 +3,16 @@ import React from 'react';
 import MiniNavBar from './mininavbar';
 import Menu from './menu';
 
-const Navigation = () => (
-  <div>
-    <MiniNavBar />
-    <Menu/>
-  </div>
-);
+class Navigation extends React.Component {
+  render() {
+    console.log(document.location.href);
+    return (
+      <div>
+        <MiniNavBar />
+        <Menu/>
+      </div>
+    )
+  }
+}
 
 export default Navigation;
